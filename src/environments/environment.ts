6e4e8f78b8d0ceafe79add5d137ef4eb5bec0cd2
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-10-24 21:31:05 -0400

    Use production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
 export const environment = {
-  production: false,
-  test: {
-    isSecure: false,
-    host: 'localhost:9001/chat',
-    apiKey: '2928818f-449a-4562-b17b-57f6373143d2'
-  }
+  production: true,
 };
