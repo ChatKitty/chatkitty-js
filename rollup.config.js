05c8a64b178f0c218f611536a4c478c230e1f721
Aaron Nwabuoku <aerisvulpe@gmail.com>
2021-11-21 03:58:17 -0500

    Fix rollup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,46 @@
-import dts from 'rollup-plugin-dts'
-import esbuild from 'rollup-plugin-esbuild'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import replace from "@rollup/plugin-replace";
+import analyze from "rollup-plugin-analyzer";
 
-const name = require('./package.json').main.replace(/\.js$/, '')
+const packageJson = require("./package.json");
 
-const bundle = config => ({
-  ...config,
-  input: './dist/index.js',
-  external: id => !/^[./]/.test(id),
-})
+const plugins = [
+  replace({
+    preventAssignment: true,
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    __buildDate__: () => JSON.stringify(new Date()),
+  }),
+  peerDepsExternal(),
+  resolve({ preferBuiltins: true }),
+  commonjs(),
+  json(),
+  typescript({ tsconfig: './tsconfig.json' }),
+  terser(),
+  analyze(),
+];
 
 export default [
-  bundle({
-    plugins: [esbuild()],
+  {
+    input: "src/index.ts",
     output: [
       {
-        file: `${name}.js`,
-        format: 'cjs',
+        file: packageJson.main,
+        format: "umd",
+        name: packageJson.name,
         sourcemap: true,
       },
       {
-        file: `${name}.mjs`,
-        format: 'es',
+        file: packageJson.module,
+        format: "esm",
         sourcemap: true,
       },
     ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: 'es',
-    },
-  }),
-]
+    plugins,
+    inlineDynamicImports: true,
+  },
+];
