99fa64e4f86af7ccb965e9479801aaad992633bb
Aaron Nwabuoku <aaron@chatkitty.com>
2021-11-22 18:48:03 -0500

    Rollup: Ignore net, tls and dns

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,13 @@ import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import replace from "@rollup/plugin-replace";
+import ignore from "rollup-plugin-ignore"
 import analyze from "rollup-plugin-analyzer";
 
 const packageJson = require("./package.json");
 
 const plugins = [
+  ignore(["net", "tls", "dns"]),
   replace({
     preventAssignment: true,
     "process.env.NODE_ENV": JSON.stringify("production"),
