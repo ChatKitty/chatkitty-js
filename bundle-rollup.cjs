993962cb1c9b724da6ffd49a067608b3d4158009
Aaron Nwabuoku <aerisvulpe@gmail.com>
2022-07-25 14:17:10 -0400

    refactor: minimize libraries

--- bundle-rollup.cjs
+++ bundle-rollup.cjs
@@ -1,7 +1,8 @@
 const url = require('@rollup/plugin-url');
 const svgr = require('@svgr/rollup');
+const uglify  = require('rollup-plugin-uglify');
 
 module.exports = options => ({
   ...options,
-  plugins: [ ...options.plugins, url(), svgr()],
+  plugins: [ ...options.plugins, url(), svgr(), uglify.uglify()],
 });
