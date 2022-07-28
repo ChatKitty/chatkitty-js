5115c69eb0a2bff2e331f388e8f33b034abadbb6
Aaron Nwabuoku <aerisvulpe@gmail.com>
2022-07-28 09:29:00 -0400

    refactor: update release branches

--- release.config.base.js
+++ release.config.base.js
@@ -1,3 +1,4 @@
 module.exports = {
   extends: 'semantic-release-npm-github-publish',
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
 };
