3013e5bab8acb03e297c3d102ad3b5f4674abf4f
Aaron Nwabuoku <aerisvulpe@gmail.com>
2023-02-06 16:53:13 -0500

    fix(core): revert build with webpack with TSC types
    This reverts commit c7dfd682bbda547d625c30056736fc1b149c2754.

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,11 +1,20 @@
-const { composePlugins, withNx } = require('@nrwl/webpack');
+const {composePlugins, withNx} = require('@nrwl/webpack');
 
-module.exports = composePlugins(withNx(), (config, { _options, _context }) => {
+const TypescriptDeclarationPlugin = require(
+  'typescript-declaration-webpack-plugin');
+
+module.exports = composePlugins(withNx(), (config, {_options, _context}) => {
   return {
     ...config,
+    plugins: [
+      new TypescriptDeclarationPlugin({
+        // Options for TypescriptDeclarationPlugin (see below)
+      }),
+      ...config.plugins
+    ],
     output: {
       ...config.output,
       globalObject: 'this',
-    },
+    }
   };
 });
