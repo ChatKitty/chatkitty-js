9f9c64ae6291ab5d0402e5efca7436805b5e958c
Aaron Nwabuoku <aerisvulpe@gmail.com>
2023-02-06 04:53:07 -0500

    fix(core): build with tsc

---
+++ packages/core/webpack.config.js
@@ -0,0 +1,20 @@
+const {composePlugins, withNx} = require('@nrwl/webpack');
+
+const TypescriptDeclarationPlugin = require(
+  'typescript-declaration-webpack-plugin');
+
+module.exports = composePlugins(withNx(), (config, {_options, _context}) => {
+  return {
+    ...config,
+    plugins: [
+      new TypescriptDeclarationPlugin({
+        // Options for TypescriptDeclarationPlugin (see below)
+      }),
+      ...config.plugins
+    ],
+    output: {
+      ...config.output,
+      globalObject: 'this',
+    }
+  };
+});
