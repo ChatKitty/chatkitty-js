8cce2bbca2714135faa4596c917480090b0d9200
Aaron Nwabuoku <aerisvulpe@gmail.com>
2023-02-28 12:48:24 -0500

    fix(core): polyfill crypto getRandomValues

--- packages/core/src/polyfills.ts
+++ packages/core/src/polyfills.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 if (typeof global.crypto !== 'object') {
   global.crypto = {
     getRandomValues: (array) => array.map(
