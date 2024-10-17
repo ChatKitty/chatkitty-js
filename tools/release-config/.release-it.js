8e6da0abf0821e29ced4b5c90259916b943481be
Aaron Nwabuoku <aaron@chatkitty.com>
2024-10-17 18:37:03 -0400

    ci: setup semantic versioning releasing

---
+++ tools/release-config/.release-it.js
@@ -0,0 +1,36 @@
+const version = '${version}';
+const packageName = process.env.npm_package_name;
+const scope = packageName.split('/')[1];
+
+module.exports = {
+	plugins: {
+		'@release-it/conventional-changelog': {
+			path: '.',
+			infile: 'CHANGELOG.md',
+			preset: 'conventionalcommits',
+			gitRawCommitsOpts: {
+				path: '.',
+			},
+		},
+	},
+	git: {
+		push: true,
+		tagName: `${packageName}-v${version}`,
+		pushRepo: 'git@github.com:b12k/monorepo-semantic-releases.git',
+		commitsPath: '.',
+		commitMessage: `feat(${scope}): released version v${version} [no ci]`,
+		requireCommits: true,
+		requireCommitsFail: false,
+	},
+	npm: {
+		publish: false,
+		versionArgs: ['--workspaces false'],
+	},
+	github: {
+		release: true,
+		releaseName: `${packageName}-v${version}`,
+	},
+	hooks: {
+		'before:git:release': ['mvm-update', 'git add --all'],
+	},
+};
